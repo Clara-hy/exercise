bc0c2988f24fcdb7d5d7a8130caedfb9a9f3280a
Clara-hy <921578272@qq.com>
2020-01-12 11:08:08 +0800

    vue-pos第一天

---
+++ Code/vue-pos/src/components/common/leftNav.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-jiesuan"></i>
+                <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-dianpu1"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-food"></i>
+                <div>商品</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-membership-card_icon"></i>
+                <div>会员</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-tongji"></i>
+                <div>统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-gongnengjianyi"></i>
+                <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.left-nav{
+    color: #fff;
+    font-size: 12px;
+    height: 100%;
+    background-color: #1d8ce0;
+    float: left;
+    width: 5%;
+}
+.iconfont{
+    font-size: 26px;
+}
+.left-nav ul{
+    padding: 0;
+    margin: 0;
+}
+.left-nav li{
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+    padding: 10px;
+}
+</style>
