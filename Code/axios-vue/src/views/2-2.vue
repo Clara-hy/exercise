45f98cd069a6479affd312c0c289f27def8c0617
Clara-hy <921578272@qq.com>
2020-01-14 15:49:26 +0800

    2-2 axios请求方法-get

---
+++ Code/axios-vue/src/views/2-2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'axios2-2',
+  components: {
+
+  },
+  created(){
+    axios.get('/data.json',{
+        params:{
+            id:12
+        }
+    }).then((res)=>{
+      console.log(res);
+    })
+    axios({
+        method:'get',
+        url:'/data.json',
+        params:{
+            id:'12'
+        }
+    }).then((res)=>{
+        console.log(res);
+    })
+  }
+}
+</script>
+ 
