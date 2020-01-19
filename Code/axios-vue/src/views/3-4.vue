a61016c6a22853c2f3059815e5b965d1f20f888e
Clara-hy <921578272@qq.com>
2020-01-19 22:53:44 +0800

    3-4.vue 取消请求

---
+++ Code/axios-vue/src/views/3-4.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  //用于取消正在进行的http请求
+  name: "axios3-4 取消请求",
+  created() {
+    let source = axios.CancelToken.source();
+    axios
+      .get("/data.json", {
+        cancelToken: source.token
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    // 取消请求，cancel方法里的参数是可选的，若为空，err则为空
+    source.cancel("cancel http");
+  }
+};
+</script>
