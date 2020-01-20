e0f185e8684499437a36dd5552b3bcbdc15d9b3d
Clara-hy <921578272@qq.com>
2020-01-20 19:37:18 +0800

    完成联系人列表的显示
    列表的显示以及获取信息出错时的提示（用到了轻提示）

--- Code/axios-vue/src/views/contactList.vue
+++ Code/axios-vue/src/views/contactList.vue
@@ -11,12 +11,13 @@
 
 <script>
 import axios from "axios";
-import { ContactList } from "vant";
+import { ContactList,Toast } from "vant";
 export default {
   name: "contactList",
   components: {
     //  组件库的按需引用
-    [ContactList.name]:ContactList
+    [ContactList.name]:ContactList,
+    [Toast .name]:Toast,
   },
   data(){
     return{
@@ -25,13 +26,22 @@ export default {
       //  name:'',
       //  tel:''
       // }
-      list:[]
+      list:[],
+      instance:null, //axios实例
     }
   },
   created() {
-    axios.get("/data.json").then(res => {
-      console.log(res);
-    });
+    this.instance = axios.create({
+      baseURL:'http://localhost:9000/api',
+      timeout:1000
+    })
+    this.instance.get('/contactList').then(res=>{
+      // console.log(res)
+      this.list = res.data.data
+    }).catch(err=>{
+      console.log(err)
+      Toast('请求失败，请稍后重试')
+    })
   },
   methods:{
     // 添加联系人
